4dc9f8d4e098bfbe727b807eff60f586948eefa8
zalajobi <zalajobi@gmail.com>
2021-11-04 06:51:03 +0100

    bug fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import Header from "../component/common/navbar/Header";
-// import Dashboard from "../component/dashboard/Dashboard";
+import Header from "../component/common/navbar/Header";
+import Dashboard from "../component/dashboard/Dashboard";
 
 const Home = (props) => {
     return (
         <React.Fragment>
-            {/*<Header/>*/}
-            <h1>HELLO ZHIKRULLAH</h1>
-            {/*<Dashboard/>*/}
+            <Header/>
+            {/*<h1>HELLO ZHIKRULLAH</h1>*/}
+            <Dashboard/>
 
 
         </React.Fragment>
